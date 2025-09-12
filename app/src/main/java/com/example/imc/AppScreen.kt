e3db360db9bb2df264ce47458076acb80a38d5dc
mataverrick <mataveraerick@gmail.com>
2025-09-11 23:21:09 -0600

    feature: initial design

--- app/src/main/java/com/example/imc/AppScreen.kt
+++ app/src/main/java/com/example/imc/AppScreen.kt
@@ -1,13 +1,86 @@
 package com.example.imc
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.imc.ui.theme.ImcTheme
 
 @Composable
-fun AppScreen(){
-
+fun AppScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.weight),
+            contentDescription = null
+        )
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = { Text("Altura") },
+            modifier = Modifier.padding(bottom = 20.dp)
+        )
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = { Text("Peso") },
+            modifier = Modifier.padding(bottom = 20.dp)
+        )
+        Button(onClick = {}, modifier = Modifier.width(350.dp).padding(start = 20.dp, end = 20.dp)) {
+            Text("Calcular")
+        }
+        OutlinedButton(onClick = {}, modifier = Modifier.width(350.dp).padding(start = 20.dp, end = 20.dp)) {
+            Text("Limpiar")
+        }
+    }
 }
 
 @Composable
-fun AppModal(){
+fun AppModal() {
 
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppPreviesw() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    ImcTheme {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    colors = topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                    ),
+                    title = {
+                        Text("Calculo de IMC")
+                    }
+                )
+            },
+        ) { innerPadding ->
+            AppScreen(modifier = Modifier.padding(innerPadding))
+        }
+    }
 }
--- app/src/main/java/com/example/imc/MainActivity.kt
+++ app/src/main/java/com/example/imc/MainActivity.kt
@@ -4,33 +4,47 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.imc.ui.theme.ImcTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ImcTheme {
-
+                Scaffold(topBar = {
+                    TopAppBar(
+                        colors = topAppBarColors(
+                            containerColor = MaterialTheme.colorScheme.primary,
+                            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                        ),
+                        title = {
+                            Text("Calculo de IMC")
+                        }
+                    )
+                }) { innerPadding ->
+                    AppScreen(modifier = Modifier.padding(innerPadding).fillMaxSize().verticalScroll(
+                        rememberScrollState()
+                    ))
+                }
             }
         }
     }
 }
 
 
-
-@Preview(showBackground = true)
-@Composable
-fun AppPreview() {
-    ImcTheme {
-        
-    }
-}
